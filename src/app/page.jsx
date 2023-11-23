bc956179c302e946fa6aaff8544d723e88a4f2f4
Gabrielllima <gabriellimamelo2302@gmail.com>
2023-11-23 19:19:37 -0300

    Estrutura componente page.jsx desenvolvido, já com classes para modules.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,95 +1,68 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className={styles.section1}>
+        <div className={styles.content}>
+          <div className={styles.leftContent}>
+            <h1>Seja bem-vindo a HaalkHealth!</h1>
+            <p><strong>O cuidado que você mais precisa
+              quando e onde quiser</strong></p>
+            <Link href="" passHref>
+              Agende sua consulta
+            </Link>
+          </div>
+          <div className={styles.rightContent}>
+            <Image src="/img/home/home1.svg" alt="" width={862} height={462} />
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className={styles.section2}>
+        <div className={styles.content}>
+          <div className={styles.leftContent}>
+            <Image src="/img/home/home2.svg" alt="" width={376} height={370} />
+          </div>
+          <div className={styles.rightContent}>
+            <h1>Por que a HaalkHealth?</h1>
+            <p><strong>Estamos aqui para auxiliá-lo(a) em suas metas de saúde no ambiente digital. Nossa abordagem híbrida possibilita a prestação de cuidados para diversas pessoas, a qualquer momento e em qualquer lugar.
+              Proporcionamos uma experiência de atendimento integrada, simplificando e tornando mais acessível o cuidado com a saúde.</strong></p>
+          </div>
+        </div>
+      </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <section className={styles.section3}>
+        <div className={styles.content}>
+          <div className={styles.leftContent}>
+            <Image src="/img/home/home3.svg" alt="" width={220} height={374} />
+          </div>
+          <div className={styles.rightContent}>
+            <h1>Além da telessaúde!</h1>
+            <p><strong>Nossa plataforma visa ir além dos limites da telessaúde convencional, criando um ecossistema poderoso interligando o atendimento digital aos cuidados presenciais, automatizados e virtuais.</strong></p>
+            <p><strong>Junto aos nossos clientes, buscamos transformar e aprimorar cada dia mais a experiência de cuidados, proporcionando  melhores resultados para todos.</strong></p>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section className={styles.section4}>
+        <div className={styles.fullWidth}>
+          <Image src="/img/home/home4_faixa.png" alt="" width={1000} height={300} />
+          <div className={styles.overlayContent}>
+            <h1>Contate-nos</h1>
+            <p><strong>Vamos falar sobre como podemos impulsionar digitalmente suas metas em relação a sua saúde.</strong></p>
+            <Link href="" passHref>
+              Contate-nos
+            </Link>
+          </div>
+          <div className={styles.overlayImage}>
+            <Image src="/img/home/home5.svg" alt="" width={300} height={200} />
+          </div>
+        </div>
+      </section>
+    </main >
   )
 }
